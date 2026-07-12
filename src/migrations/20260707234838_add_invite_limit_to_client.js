facc1bbf609d5b0dbc1b6ee52f3f5eafc27995b5
PI-Engineering-LLC <contactus@piengineeringllc.com>
2026-07-12 17:05:19 -0500

    remove migrations

--- src/migrations/20260707234838_add_invite_limit_to_client.js
+++ src/migrations/20260707234838_add_invite_limit_to_client.js
@@ -1,19 +1,19 @@
-// /**
-//  * @param { import("knex").Knex } knex
-//  * @returns { Promise<void> }
-//  */
-// exports.up = function(knex) {
-//     return knex.schema.alterTable('clients', function(table) {
-//         table.integer('invite_limit').notNullable().defaultTo(5);
-//     });
-// };
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    // return knex.schema.alterTable('clients', function(table) {
+    //     table.integer('invite_limit').notNullable().defaultTo(5);
+    // });
+};
 
-// /**
-//  * @param { import("knex").Knex } knex
-//  * @returns { Promise<void> }
-//  */
-// exports.down = function(knex) {
-//     return knex.schema.alterTable('clients', function(table) {
-//         table.dropColumn('invite_limit');
-//     });
-// };
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    // return knex.schema.alterTable('clients', function(table) {
+    //     table.dropColumn('invite_limit');
+    // });
+};
