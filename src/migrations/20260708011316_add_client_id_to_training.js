2629d3a9b2385f490b4f50220ce969eb5de5504f
PI-Engineering-LLC <contactus@piengineeringllc.com>
2026-07-12 17:03:35 -0500

    remove migrations

--- src/migrations/20260708011316_add_client_id_to_training.js
+++ src/migrations/20260708011316_add_client_id_to_training.js
@@ -1,20 +1,20 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  //t.uuid('client_id').notNullable().references('id').inTable('clients').onDelete('CASCADE');;
-  return knex.schema.alterTable('training_sessions', function(table) {
-    table.uuid('client_id').notNullable().references('id').inTable('clients').onDelete('CASCADE');
-});
-};
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+// exports.up = function(knex) {
+//   //t.uuid('client_id').notNullable().references('id').inTable('clients').onDelete('CASCADE');;
+//   return knex.schema.alterTable('training_sessions', function(table) {
+//     table.uuid('client_id').notNullable().references('id').inTable('clients').onDelete('CASCADE');
+// });
+// };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-    return knex.schema.alterTable('training_sessions', function(table) {
-        table.dropColumn('client_id');
-    });
-};
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+// exports.down = function(knex) {
+//     return knex.schema.alterTable('training_sessions', function(table) {
+//         table.dropColumn('client_id');
+//     });
+// };
